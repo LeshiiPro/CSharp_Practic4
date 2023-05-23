411e3a9e71139079f15e751f15ff467434e03bef
LeshiiPro <alexpozdnyak@yandex.ru>
2023-05-23 19:50:38 +0300

    Lesson_4.1

---
+++ CSharp_Practic4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("You color is red!");
+            Console.ReadKey();
+
+        }
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("You color is green!");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("You color is Cyan");
+            Console.ReadKey();
+
+        }
+    }
+}
+
